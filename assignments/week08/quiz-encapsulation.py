71d1469ff0590a6b84c5742bf3b019c7b4a4a29e
Panthita Khaokhrua <pantita.kha@ku.th>
2025-09-16 14:56:25 +0700

    labweek 8

--- assignments/week08/quiz-encapsulation.py
+++ assignments/week08/quiz-encapsulation.py
@@ -6,4 +6,28 @@ Methods to calculate area (getArea()) and perimeter getPerimeter())
 A method to check if it's a square (isSquare())
 
 """
+
+
+class Rectangle:
+    def __init__(self, length, width):
+        self.__length = length
+        self.__width = width
+ 
+    def getArea(self):
+        return self.__length * self.__width
+ 
+    def getPerimeter(self):
+        return 2 * (self.__length + self.__width)
+ 
+    def isSquare(self):
+        return self.__length == self.__width
+ 
+ 
+rect1 = Rectangle(10, 5)
+print("Area:", rect1.getArea())     
+print("Perimeter:", rect1.getPerimeter()) 
+print("Is Square?", rect1.isSquare())
+ 
+rect2 = Rectangle(7, 7)
+print("Is Square?", rect2.isSquare())
     
